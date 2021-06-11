c81eab0c3245bffc619f1ada97880b04e0e8e278
Akeem King <akeemtlking@gmail.com>
2021-06-10 21:40:00 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask.json import jsonify
+from markupsafe import escape
+from flask import render_template, Flask, request
+from twint import run, Config
+
+
+def marshal_tweet(tweet):
+	return {
+		'id': tweet.id,
+		'user_id': tweet.user_id,
+		'username': tweet.username,
+		'content': tweet.tweet
+	}
+
+batch_size = 20 # twint uses increments of 20
+default_limit = 1 * batch_size 
+def grab_tweets(username, filter, limit = default_limit):
+	tweets = []
+	run.Search(Config(
+		Limit=int(limit),
+		Search=filter,
+		Username=username,
+		Hide_output=True,
+		Store_object=True,
+		Store_object_tweets_list=tweets,
+	))
+	return [marshal_tweet(tweet) for tweet in tweets]
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('main.html')
+
+@app.route("/tweets")
+def tweets():
+	return render_template('tweets.html')
+
+@app.route("/search/tweets")
+def get_tweets():
+	username = escape(request.args.get('username', '', type=str))
+	limit = escape(request.args.get('limit', '', type=str))
+	filter = escape(request.args.get('filter', '', type=str))
+	tweets = grab_tweets(username=username, limit=limit, filter=filter)
+	return jsonify(tweets)
