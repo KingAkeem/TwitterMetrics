5e8f948c46b3b08ecca7e165c52be3c1a2d6de60
Akeem King <akeemtlking@gmail.com>
2021-06-17 02:05:14 -0400

    Adding rudimentary export button

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+from flask import json
 from flask.json import jsonify
 from markupsafe import escape
-from flask import render_template, Flask, request
-from twint import run, Config
-
+from flask import render_template, Flask, request, Response
+from twint import run, Config 
 
 def marshal_tweet(tweet):
 	return {
@@ -10,6 +10,13 @@ def marshal_tweet(tweet):
 		'user_id': tweet.user_id,
 		'username': tweet.username,
 		'content': tweet.tweet
+	}
+
+def marshal_user(user):
+	return {
+		'id': user.id,
+		'username': user.username,
+		'icon': user.avatar
 	}
 
 batch_size = 20 # twint uses increments of 20
@@ -26,6 +33,15 @@ def grab_tweets(username, filter, limit = default_limit):
 	))
 	return [marshal_tweet(tweet) for tweet in tweets]
 
+def grab_user(username):
+	users = []
+	run.Lookup(Config(
+		Username=username,
+		Store_object=True,
+		Store_object_users_list=users
+	))
+	return marshal_user(user=users.pop())
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -36,6 +52,17 @@ def main():
 def tweets():
 	return render_template('tweets.html')
 
+@app.route("/user")
+def followers():
+	return render_template('user.html')
+
+@app.route("/export/pdf", methods=['post'])
+def export_pdf():
+	rows = json.loads(request.form.get('rows'))
+	for row in rows:
+		print(row)
+	return Response(status=200)
+
 @app.route("/search/tweets")
 def get_tweets():
 	username = escape(request.args.get('username', '', type=str))
@@ -43,3 +70,9 @@ def get_tweets():
 	filter = escape(request.args.get('filter', '', type=str))
 	tweets = grab_tweets(username=username, limit=limit, filter=filter)
 	return jsonify(tweets)
+
+@app.route("/search/user")
+def get_user():
+	username = escape(request.args.get('username', '', type=str))
+	users = grab_user(username=username)
+	return jsonify(users)
