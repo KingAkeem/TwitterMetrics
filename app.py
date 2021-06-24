118dd51ce7fb26c1cfbbd90d322c9284b42f5747
Akeem King <akeemtlking@gmail.com>
2021-06-24 13:05:09 -0400

    Add CSV download capability

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import csv
+
 from flask import json
 from flask.json import jsonify
 from markupsafe import escape
-from flask import render_template, Flask, request, Response
+from flask import render_template, Flask, request, Response, make_response
 from twint import run, Config 
 
 def marshal_tweet(tweet):
@@ -56,12 +58,37 @@ def tweets():
 def followers():
 	return render_template('user.html')
 
-@app.route("/export/pdf", methods=['post'])
-def export_pdf():
+
+def create_csv(name, rows):
+	filename = f'{name}.csv'
+	with open(filename, 'w+') as csvfile:
+		csvwriter = csv.DictWriter(csvfile, fieldnames=['ID', 'Tweets'])
+		csvwriter.writeheader()
+		for row in rows:
+			try:
+				csvwriter.writerow({
+					'ID': row['id'],
+					'Tweets': row['content']
+				})
+			except KeyError as e:
+				print(f'Found invalid row. {e}')
+		return filename 
+
+
+
+@app.route("/export/<file_type>", methods=['post'])
+def export(file_type):
+	username = escape(request.args.get('username', '', type=str))
 	rows = json.loads(request.form.get('rows'))
-	for row in rows:
-		print(row)
-	return Response(status=200)
+
+	if file_type == 'csv':
+		filename = create_csv(username, rows)
+		with open(filename, 'rb') as csv_file:
+			response = make_response(csv_file.read())
+			response.status = 200
+			response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+			response.headers["Content-type"] = "text/csv"
+			return response
 
 @app.route("/search/tweets")
 def get_tweets():
